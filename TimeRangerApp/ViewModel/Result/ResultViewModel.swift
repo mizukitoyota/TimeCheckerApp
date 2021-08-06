8f941500ac87bf73410b3acc42ae2c6e8b3cb30f
mizukitoyota <mtoyota@system-renovete.co.jp>
2021-08-06 09:26:57 +0900

    関数名変更

---
+++ TimeRangerApp/ViewModel/Result/ResultViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ResultViewModel.swift
+//  TimeRangerApp
+//
+//  Created by SR-Toyota on 2021/08/02.
+//
+
+import Foundation
+
+class ResultViewModel {
+    func setTabeleDate() {
+        
+    }
+}
